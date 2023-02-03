ac613b017d28124313ab74f0367c6f7e71f92ac0
MohanYeg <yeguru0@gmail.com>
2023-02-03 12:46:26 +0530

    second commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mohan Yeguru
+ *
+ */
+module DemoGitProject {
+}
---
+++ src/testpackage/GitTest.java
@@ -0,0 +1,9 @@
+package testpackage;
+
+public class GitTest {
+	public static void main(String args[]) {
+	int a=10,b=87;
+	int c=a+b;
+	System.out.println("c="+c);
+}
+}
